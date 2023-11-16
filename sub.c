0d44ad80e1dfd113cd52cfae862aa46b9b4a3349
Abderrahmane Benjelloun <benjelloun857@gmail.com>
2023-11-16 22:51:04 +0100

    task_sub

---
+++ sub.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+
+/**
+ * sub - perform sub operation
+ * @stack: pointer to pointer to the stack  head
+ * @line_number: the line number
+ *
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp = NULL;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		dprintf(2, "L%d: can't sub, stack too short\n", line_number);
+		code = -1;
+	}
+	else
+	{
+		temp = *stack;
+		temp->next->n = temp->next->n - temp->n;
+		*stack = (*stack)->next;
+		(*stack)->prev = NULL;
+		free(temp), temp = NULL;
+	}
+}
